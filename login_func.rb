19c8668ce230b67c00b1541f305e8137e6981e7f
billyjack1988 <billyjacktattoos@gmail.com>
2017-10-24 16:00:20 -0500

    updated the database

--- login_func.rb
+++ login_func.rb
@@ -1,3 +1,7 @@
+require 'pg'
+load './local_env.rb' if File.exist?('./local_env.rb')
+
+
 def check_creds?(user,pass)
 	db_params = {
 	host: ENV['host'],
@@ -8,7 +12,7 @@ def check_creds?(user,pass)
 }
 db = PG::Connection.new(db_params)
  
-check = db.exec("SELECT*FROM userandpassword WHERE username = '#{user}'")
+check = db.exec("SELECT*FROM username_password WHERE user_name = '#{user}'")
  		if check.num_tuples.zero? == false
  			check_val = check.values.flatten
   			if check_val[1] == pass
@@ -30,13 +34,38 @@ def add_to_login(user,pass)
 	password: ENV['password']
 }
 db = PG::Connection.new(db_params)
- check = db.exec("SELECT*FROM userandpassword WHERE username = '#{user}'")
+ check = db.exec("SELECT*FROM username_password WHERE user_name = '#{user}'")
 	message = ""
 	if check.num_tuples.zero? == false
 		message = "Username Already Taken"
 	else
 		message = "Login Created"
-	db.exec("insert into userandpassword(username,password)VALUES('#{user}','#{pass}')")
+	db.exec("insert into username_password(user_name,pass_word)VALUES('#{user}','#{pass}')")
 	end
 	message
 end
+
+def hard_func(user, pass, admin)
+    db_params = {
+        host: ENV['host'],
+        port: ENV['port'],
+        dbname: ENV['dbname'],
+        user: ENV['user'],
+        password: ENV['password']
+    }
+db = PG::Connection.new(db_params)
+     
+answer = ""
+check = db.exec("SELECT * FROM username_password WHERE user_name = '#{user}'")
+
+   if check.num_tuples.zero? == false
+        answer = "Your Number is already being used"
+    else
+        answer = "you joined this phone book"
+        db.exec("insert into username_password(user_name, pass_word, admin)VALUES('#{user}','#{pass}', '#{admin}')")
+    end
+    answer
+
+end
+hard_func("test", "1234", "no")
+    
