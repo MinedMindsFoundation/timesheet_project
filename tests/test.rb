5bdf813786604ba05f4286cd38d19850639feb60
2staffi@gmail.com <2staffi@gmail.com>
2017-10-25 15:37:33 -0400

    new rake task

--- tests/test.rb
+++ tests/test.rb
@@ -11,7 +11,7 @@ class ThrivyTestCase < Test::Unit::TestCase
      @driver = Selenium::WebDriver.for:chrome, desired_capabilities:caps
     target_size = Selenium::WebDriver::Dimension.new(768, 894)
     @driver.manage.window.size = target_size
-    @driver.navigate.to("https://developmentheythrivy.herokuapp.com/")
+    @driver.navigate.to("http://wvtimeclock.herokuapp.com/")
     @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
   end  
 
