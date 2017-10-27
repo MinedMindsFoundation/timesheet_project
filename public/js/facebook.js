d23860429a5288fe6554f04d0752eb2615c45eb2
Ldeems <luke.deems@yahoo.com>
2017-10-27 17:56:35 -0400

    working twards getting info

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -43,7 +43,7 @@ function FBLogin() {
                 //                    document.getElementById('email').value = email;
                 //                    document.getElementById('fb_id').value = fb_id;
 
-                window.location = "next_page";
+                window.location = "to_landing";
 
             });
 
@@ -66,12 +66,12 @@ function FBLogin2() {
                 var last_name = response.last_name;
                 var email = response.email;
                 email = response.email;
-                //                    var fb_id = response.id;
-                //                    document.getElementById('first_name').value = first_name;
-                //                    document.getElementById('last_name').value = last_name;
-                //                    document.getElementById('email').value = email;
-                //                    document.getElementById('fb_id').value = fb_id;
-                window.location = "/next_page";
+                                   var fb_id = response.id;
+                                   document.getElementById('first_name').value = first_name;
+                                   document.getElementById('last_name').value = last_name;
+                                   document.getElementById('email').value = email;
+                                   document.getElementById('fb_id').value = fb_id;
+                window.location = "/to_landing";
 
             });
 
@@ -82,25 +82,25 @@ function FBLogin2() {
     }, { scope: 'public_profile, email' });
 }
 
-//    function getUserInfo() {
-//        FB.api('/me?fields=id, first_name, last_name, email', function(response){
-//            //alert(response.first_name + " " + response.last_name + " " + response.email + " " + response.id);
-//            var first_name = response.first_name,
-//                    last_name = response.last_name,
-//                    email = response.email;
-//
-//            document.getElementById('email').value = email;
-//            document.getElementById('first_name').value = first_name;
-//            document.getElementById('last_name').value = last_name;
-//        });
-//    }
-//    function getPhoto()
-//    {
-//        FB.api('/me/picture?type=normal', function(response) {
-//            var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
-//            document.getElementById("status").innerHTML+=str;
-//        });
-//    };
+   function getUserInfo() {
+       FB.api('/me?fields=id, first_name, last_name, email', function(response){
+           //alert(response.first_name + " " + response.last_name + " " + response.email + " " + response.id);
+           var first_name = response.first_name,
+                   last_name = response.last_name,
+                   email = response.email;
+
+           document.getElementById('email').value = email;
+           document.getElementById('first_name').value = first_name;
+           document.getElementById('last_name').value = last_name;
+       });
+   }
+   function getPhoto()
+   {
+       FB.api('/me/picture?type=normal', function(response) {
+           var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
+           document.getElementById("status").innerHTML+=str;
+       });
+   };
 function logout() {
     FB.logout(function(response) {
         // user is now logged out
