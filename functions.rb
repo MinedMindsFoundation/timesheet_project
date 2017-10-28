d8c3b7601a7f184871e70b994bdd0c73305f18de
ScStew <scottmstewart2@gmail.com>
2017-10-28 18:51:19 -0400

    updated submit-time-in to work with updated database

--- functions.rb
+++ functions.rb
@@ -33,24 +33,24 @@ def submit_time_in(user_id,time)
         }
         db = PG::Connection.new(db_params)
     
-    db.exec("INSERT INTO timesheet(user_id,clock_in)VALUES('#{user_id}','#{time}')")
+    db.exec("INSERT INTO timesheet_#{user_id}(clock_in,clock_out,date)VALUES('#{time[1]}','N/A','#{time}')")
 end
 
-def add_info(user_id,email,first_name,last_name,admin)
-db_params = {
-    host: ENV['host'],
-    port: ENV['port'],
-    dbname: ENV['dbname'],
-    user: ENV['user'],
-    password: ENV['password']
-    }
-    db = PG::Connection.new(db_params)
+# def add_info(user_id,email,first_name,last_name,admin)
+# db_params = {
+#     host: ENV['host'],
+#     port: ENV['port'],
+#     dbname: ENV['dbname'],
+#     user: ENV['user'],
+#     password: ENV['password']
+#     }
+#     db = PG::Connection.new(db_params)
     
-    db.exec("insert into names(user_id,first_name,last_name)VALUES('#{user_id}','#{first_name}','#{last_name}')")
-    db.exec("insert into emails(user_id,emails)VALUES('#{user_id}','#{email}')")
-    db.exec("insert into admin(user_id,admin)VALUES('#{user_id}','#{admin}')")
+#     db.exec("insert into names(user_id,first_name,last_name)VALUES('#{user_id}','#{first_name}','#{last_name}')")
+#     db.exec("insert into emails(user_id,emails)VALUES('#{user_id}','#{email}')")
+#     db.exec("insert into admin(user_id,admin)VALUES('#{user_id}','#{admin}')")
 
-end
+# end
 
 def add_user(user_id,email,first_name,last_name,pto,admin,doh)
     db_params = {
@@ -66,5 +66,3 @@ def add_user(user_id,email,first_name,last_name,pto,admin,doh)
     db.exec("create table timesheet_#{user_id} (time_in text,time_out text,date text)")
 
 end
-
-add_user("scstew","abearkin@hotmail.com","Scott","Stewart","0","N","N/A")
