015e71afed23777695b304086cb92ecb4613a0b8
Ldeems <luke.deems@yahoo.com>
2017-10-28 14:59:09 -0400

    missed a merge accept

--- app.rb
+++ app.rb
@@ -12,18 +12,6 @@ erb :login
 end
 
 # comming from login.erb
-<<<<<<< HEAD
-post "/login" do
-first_name = params[:first_name]
-last_name = params[:last_name]
-email = params[:email]
-p first_name
-p last_name
-p email
-redirect "/to_landing?"
-end
-
-=======
 post '/login' do 
 session[:first_name] = params[:first_name]
 session[:last_name] = params[:last_name]
@@ -34,7 +22,6 @@ end
 
 
 
->>>>>>> 3d1e9fa702343209046f72124c6131b6efe6d008
 # leads to landing page 
 get "/to_landing" do
 erb :landing
