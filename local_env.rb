d5d39c07089f3a3e2b91704e92f28d82e0d289a9
ScStew <scottmstewart2@gmail.com>
2017-10-29 20:34:05 -0400

    checking env

--- local_env.rb
+++ local_env.rb
@@ -2,6 +2,6 @@ ENV['host'] 		= 'sms-database-instance.czzyuhkiyje7.us-east-2.rds.amazonaws.com'
 ENV['port'] 		= '5432'
 ENV['dbname'] 		= 'sms_database'
 ENV['user'] 		= 'smsbearkin'
-ENV['password'] 	= 'Beet2ivo7'
+ENV['password'] 	= ''
 
 
