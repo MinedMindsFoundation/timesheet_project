fbcdbfca635e32d03a3677b41de4d1875f931778
ScStew <scottmstewart2@gmail.com>
2017-10-29 20:16:49 -0400

    added message for when clocking in and out is successfull and unsuccessfull

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ load './local_env.rb' if File.exist?('./local_env.rb')
 
 # Initial "get" leads to login page
 get "/" do 
+session[:message] = ''
 erb :login
 end
 
@@ -33,15 +34,15 @@ end
 
 # post comming from landing page
 post "/clock_in" do
+    
     if time_in_check?(session[:user_id])
         time = get_time()
         submit_time_in(session[:user_id],time[0],time[1])
-         "time in submitted"
-
+        session[:message] = "Time in Submitted"
     else
-        "time in not submitted"
+        session[:message] = "Already Submitted Time In"
     end
-    
+    redirect "/to_landing"
  end
 
 # post comming from landing page
@@ -49,10 +50,10 @@ post "/clock_out" do
     if time_out_check?(session[:user_id])
         time = get_time()
         submit_time_out(session[:user_id],time[0])
-         "time out submitted"
+        session[:message] = "Time Out Submitted"
     else
-         "time out not submitted"
+        session[:message] =  "Already Submitted Time Out"
     end
-
+    redirect "/to_landing"
 end
 
