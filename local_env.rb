bf9589c38fc2e757c6197957ca847bcabf79b548
ScStew <scottmstewart2@gmail.com>
2017-10-30 01:47:43 -0400

    remaking gitignore

--- local_env.rb
+++
@@ -1,7 +0,0 @@
-ENV['host'] 		= 'sms-database-instance.czzyuhkiyje7.us-east-2.rds.amazonaws.com'
-ENV['port'] 		= '5432'
-ENV['dbname'] 		= 'sms_database'
-ENV['user'] 		= 'smsbearkin'
-ENV['password'] 	= ''
-
-
