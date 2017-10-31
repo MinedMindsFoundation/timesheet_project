40ac3ae3056d832655682eb81bba94e5f184a524
billyjack1988 <billyjacktattoos@gmail.com>
2017-10-31 10:42:23 -0500

    working on the email javascript

---
+++ public/js/email.js
@@ -0,0 +1,16 @@
+// function sendMail() {
+//     var link = "mailto:billyjacktattoos@gmail.com" +
+//         "?cc=fakeemail@junkmail.com" +
+//         "&subject=" + escape("subject") +
+//         "&body=" + escape(document.getElementById('myText').value);
+
+//     window.location.href = link;
+// }
+
+
+function sendemail() {
+    var email = document.getElementById("emailID").value;
+    var subject = ('PTO Request');
+    var body = ('My permanent body contents');
+    document.write('<a href="mailto:billyjacktattoos@gmail.com' + email + '?subject=' + subject + '&body=' + body + '">' + 'Click here to send email as well' + '<' + '/a>');
+}
