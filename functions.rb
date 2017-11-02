4bba2ea56cfb5118b56e3586762cc7bdf7388c3e
garrekmontantez <runecryptmc@gmail.com>
2017-11-02 17:21:19 -0400

    Added function to bring user list down, working on it.

--- functions.rb
+++ functions.rb
@@ -211,11 +211,23 @@ def database_email_check(user_id)
         user_email.flatten.first
 end
 
-
-
-
-
-
+def admin_emp_list
+    db_params = {
+        host: ENV['host'],
+        port: ENV['port'],
+        dbname: ENV['dbname'],
+        user: ENV['user'],
+        password: ENV['password']
+        }
+        db = PG::Connection.new(db_params)
+        users = db.exec("SELECT user_id, first_name, last_name, admin FROM info").values
+        emails = db.exec("SELECT email FROM email").values
+        db.close
+        emp_arr = []
+        emp_arr << users.flatten
+        emp_arr << emails.flatten
+        emp_arr
+end
 
 def add_email(user_id,email)
     db_params = {
