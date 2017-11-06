12520e7d732a93a8cb0a4ae41f97053af1c07f86
billyjack1988 <billyjacktattoos@gmail.com>
2017-11-06 10:54:40 -0500

    got the alearts for clockin and out and lunch alerts

---
+++ public/js/alert_box.js
@@ -0,0 +1,15 @@
+function clockedINfunction() {
+    alert("You are clocked in.");
+}
+
+function clockedOUTfunction() {
+    alert("You are clocked out.");
+}
+
+function lucnchINfunction() {
+    alert("You are clocked out for lunch.");
+}
+
+function lunchOUTfunction() {
+    alert("You are back from lunch.");
+}
