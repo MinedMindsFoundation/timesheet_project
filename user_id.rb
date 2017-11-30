793b25344a6313e9a310a13598051d8b7794a90a
ScStew <scottmstewart2@gmail.com>
2017-11-30 06:31:25 -0500

    passed first test in user)info_test.rb

--- user_id.rb
+++ user_id.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 class User
     
     attr_accessor :users_list ,:user_data
@@ -10,9 +12,9 @@ class User
             user: ENV['user'],
             password: ENV['password']
         }
-        @db = PG::Connection.new(db_params)
-        @users_list = db.exec("Select user_id From info.new").values
-        @user_data = db.exec("Select info_new.*,admin_status.* From info_new,admin_status WHERE user_id = #{user_id}")
+        @db = PG::Connection.new(@db_params)
+        @users_list = @db.exec("Select user_id From info_new").values
+        @user_data = @db.exec("Select info_new.*,admin_status.admin, admin_status.hierarchy From info_new,admin_status WHERE admin_status.user_id = '#{user_id}' And info_new.user_id = '#{user_id}'").values.flatten
     end
         
 end
--- user_info_tests.rb
+++ user_info_tests.rb
@@ -5,6 +5,6 @@ load 'local_env.rb' if File.exist?('local_env.rb')
 class Test_funcs < Minitest::Test
     def test_get_user_info
         user_info = User.new("scottid")
-        assert_equal(["scottid","Scott","Stewart","No","1"],user_info.user_data)
+        assert_equal(["scottid","Scott","Steward","No","1"],user_info.user_data)
     end
 end
