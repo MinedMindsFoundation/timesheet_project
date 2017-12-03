4a31a1b255f3f62f9ddbbee1d11cb73906972d48
Ldeems <luke.deems@yahoo.com>
2017-12-02 22:09:38 -0500

    fifth test for timebuildup made and passing

--- tests/test_functions.rb
+++ tests/test_functions.rb
@@ -1597,4 +1597,18 @@ class Test_funcs < Minitest::Test
         x = timeoffbiuldup(user_id,user_info,user_pto,hire_date,pto_stamp,user_vac,user_sic,todays_year_stamp,todays_month_stamp)
         assert_equal("days were added 2orlonger",x)
     end 
+
+    def test_timeoffbiuldup_5
+        user_id = "bradyid"
+        user_info = "none"
+        user_pto = "2"
+        hire_date = ["2001-9-28"]
+        pto_stamp = ["2017 12"]
+        user_vac = "2"
+        user_sic = "2"
+        todays_year_stamp = "2017"
+        todays_month_stamp = "12"
+        x = timeoffbiuldup(user_id,user_info,user_pto,hire_date,pto_stamp,user_vac,user_sic,todays_year_stamp,todays_month_stamp)
+        assert_equal("days have already been added",x)
+    end
 end
