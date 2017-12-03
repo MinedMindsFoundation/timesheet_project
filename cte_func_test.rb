da606173398a3126b45157925dc40f40318735ae
ScStew <scottmstewart2@gmail.com>
2017-12-02 22:16:40 -0500

    added tests for get_names in cte_func.rb

--- cte_func_test.rb
+++ cte_func_test.rb
@@ -7,5 +7,24 @@ class Test_cte_funcs < Minitest::Test
         assert_equal(1,1)
     end
 
+    def test_get_supervisees_return_array
+        user_id = "scottid"
+        assert_equal(Array,get_supervisees(user_id).class)
+    end
+
+    def test_get_supervisees_return_arr_of_strings
+        user_id = "scottid"
+        assert_equal(String,get_supervisees(user_id).first.class)
+    end
+
+    def test_for_get_names_return_arr
+        id_arr = ['scottid']
+        assert_equal(Array,get_names(id_arr).class)
+    end
+
+    def test_get_names_return_multi_d_arr
+        id_arr = ["scottid"]
+        assert_equal(Array,get_names(id_arr).first.class)
+    end
 
 end
