2051c9662b179f79ecd841087634703ebc423976
ScStew <scottmstewart2@gmail.com>
2018-01-02 14:33:20 -0500

    Testing that InvoiceSpreadsheet class gets access to template

--- spreadsheet.rb
+++ spreadsheet.rb
@@ -2,22 +2,22 @@ require 'rubyXL'
 
 class InvoiceSpreadsheet
 
-    def initialize()
+    def initialize() # parses invoice template on startups and sets first worksheet to an instance variable
     workbook = RubyXL::Parser.parse("./public/template/template.xlsx")
     @worksheet = workbook[0]
     end
 
-    def input_data(location,input)
+    def input_data(location,input) # Changes the value of the cell(location) to input
     loc = RubyXL::Reference.ref2ind(location)
     # p "#{x[loc[0]][loc[1]].value} before"
     @worksheet[loc[0]][loc[1]].change_contents(input, @worksheet[loc[0]][loc[1]].formula)
     # p "#{x[loc[0]][loc[1]].value} after"
     end
 
-    def show_data(location)
+    def show_data(location) # Returns the value of the cell selected
         loc = RubyXL::Reference.ref2ind(location)
-        p @worksheet[loc[0]][loc[1]]
-        @worksheet[loc[0]][loc[1]]
+        p @worksheet[loc[0]][loc[1]].value
+        @worksheet[loc[0]][loc[1]].value
     end
 end
 
--- spreadsheet_test.rb
+++ spreadsheet_test.rb
@@ -5,6 +5,14 @@ class Test_spreadsheet < Minitest::Test
     def test_1_and_1
         assert_equal(1,1)
     end
-    def test_grabbing_template
+    
+    def test_accessing_template_1
+        x = InvoiceSpreadsheet.new()
+        assert_equal("Description",x.show_data("C7"))
+    end
+
+    def test_accession_template_2
+        x = InvoiceSpreadsheet.new()
+        assert_equal("Name:",x.show_data("B4"))
     end
 end
