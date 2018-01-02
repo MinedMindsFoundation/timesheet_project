5ec11656b4b15c9e5aa4ab63bf54fe32dfff5f87
ScStew <scottmstewart2@gmail.com>
2018-01-02 13:51:40 -0500

    turned spreadsheet into class and made function to change info values within spreadsheet

--- spreadsheet.rb
+++ spreadsheet.rb
@@ -1,15 +1,19 @@
 require 'rubyXL'
 
+class InvoiceSpreadsheet
 
-def get_template(temp)
-    workbook = RubyXL::Parser.parse("./public/template/" + temp + '.xlsx')
-    worksheet= workbook[0]
-    worksheet.each { |row|
-   row && row.cells.each { |cell|
-     val = "#{cell} && #{cell.value}"
-     p val
-   }
-}   
+    def initialize()
+    workbook = RubyXL::Parser.parse("./public/template/template.xlsx")
+    @worksheet = workbook[0]
+    end
+
+    def input_data(location,input)
+    loc = RubyXL::Reference.ref2ind(location)
+    # p "#{x[loc[0]][loc[1]].value} before"
+    @worksheet[loc[0]][loc[1]].change_contents(input, @worksheet[loc[0]][loc[1]].formula)
+    # p "#{x[loc[0]][loc[1]].value} after"
+    end
+
 end
 
-get_template("template")
+InvoiceSpreadsheet.new().input_data("A1",1)
