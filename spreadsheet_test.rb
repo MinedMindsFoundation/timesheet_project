9bf7ecba5d74a0c0e8d5b18c12bece455da3421a
ScStew <scottmstewart2@gmail.com>
2018-01-02 14:21:17 -0500

    initial testing enviroment setup

---
+++ spreadsheet_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative 'spreadsheet.rb'
+
+class Test_spreadsheet < Minitest::Test
+    def test_1_and_1
+        assert_equal(1,1)
+    end
+    def test_grabbing_template
+    end
+end
