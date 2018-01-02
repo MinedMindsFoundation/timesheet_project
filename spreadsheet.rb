d7c5257bf9030b1163d0efba094f4df592b7b2cf
ScStew <scottmstewart2@gmail.com>
2018-01-02 17:01:52 -0500

    Made method to mail invoice inside of InvoiceSpreadsheet Class

--- spreadsheet.rb
+++ spreadsheet.rb
@@ -1,10 +1,11 @@
 require 'rubyXL'
-
+require "mail"
+load './local_env.rb' if File.exist?('./local_env.rb')
 class InvoiceSpreadsheet
 
     def initialize() # parses invoice template on startups and sets first worksheet to an instance variable
-    workbook = RubyXL::Parser.parse("./public/template/template.xlsx")
-    @worksheet = workbook[0]
+    @workbook = RubyXL::Parser.parse("./public/template/template.xlsx")
+    @worksheet = @workbook[0]
     end
 
     def input_data(location,input) # Changes the value of the cell(location) to input
@@ -19,6 +20,44 @@ class InvoiceSpreadsheet
         p @worksheet[loc[0]][loc[1]].value
         @worksheet[loc[0]][loc[1]].value
     end
+
+    def generate_new_file(name,date)
+        @workbook.write(name + " " + date + " " + "invoice.xlsx")
+    end
+
+    def delete_file(name,date)
+       File.delete(name + " " + date + " " + "invoice.xlsx")
+    end
+
+    def mail_invoice(email,name,date)
+        generate_new_file(name,date)
+        path = File.absolute_path(name + " " + date + " " + "invoice.xlsx")
+        p path
+        Mail.defaults do
+            delivery_method :smtp,
+            address: "email-smtp.us-east-1.amazonaws.com",
+            port: 587,
+            :user_name  => ENV['a3smtpuser'],
+            :password   => ENV['a3smtppass'],
+            :enable_ssl => true
+          end
+            email_body = "#{name} invoice" 
+                mail = Mail.new do
+                from         ENV['from']
+                to           'scottmstewart2@gmail.com'
+                subject      "PTO Request with no days to request"
+                add_file path
+        
+            html_part do
+                content_type 'text/html'
+                body       email_body
+            end
+        end
+          mail.deliver!
+
+         delete_file(name,date)
+    end
+
 end
 
 
--- spreadsheet_test.rb
+++ spreadsheet_test.rb
@@ -26,4 +26,54 @@ class Test_spreadsheet < Minitest::Test
         assert_nil(x.show_data("A1"))
     end
 
+    def test_input_data
+        input = 12
+        loc = "A1"
+        x = InvoiceSpreadsheet.new()
+        x.input_data(loc,input)
+        assert_equal(input,x.show_data(loc))
+    end
+
+    def test_input_data_a1_is_12
+        input = 12
+        loc = "A1"
+        x = InvoiceSpreadsheet.new()
+        x.input_data(loc,input)
+        assert_equal(input,x.show_data(loc))
+    end
+
+
+    def test_input_data_a1_is_word
+        input = "word"
+        loc = "A1"
+        x = InvoiceSpreadsheet.new()
+        x.input_data(loc,input)
+        assert_equal(input,x.show_data(loc))
+    end
+
+
+    def test_input_data_c4_is_32_string
+        input = "32"
+        loc = "C4"
+        x = InvoiceSpreadsheet.new()
+        x.input_data(loc,input)
+        assert_equal(input,x.show_data(loc))
+    end
+
+    # def test_for_generating_new_file
+    #     x = InvoiceSpreadsheet.new()
+    #     x.input_data("A1","NEW FILE TEST")
+    #     x.generate_new_file("TEST","1-1-2018")
+    #     p "it ran"
+    #     x.delete_file("TEST","1-1-2018")
+    # end
+
+
+    def test_for_mail_invoice
+        x = InvoiceSpreadsheet.new()
+        x.input_data("A1","TEST WOO")
+        x.mail_invoice("TEST@test.com", "TEST","1-1-2018")
+        p "it did it"
+    end
+
 end
